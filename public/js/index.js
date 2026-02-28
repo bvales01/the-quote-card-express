22fceb1b9464ac2773422f0eaf44e5440dfcb20a
Brianna <briannavales7@gmail.com>
2026-02-28 13:56:38 -0500

    Finalize CORS and hide API key

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,22 @@ const elements = {
 };
 
 async function getRandomImage() {
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.data;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.computedStyleMap.backgroundImage = `url(${receivedPhotoUrl})`;
+    } catch (error) {
+        console.error(error);
+    };
+};
+
+getRandomImage();
+
+/* async function getRandomImage() {
     const client_id = "your_access_key";
     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
     try {
@@ -20,7 +36,7 @@ async function getRandomImage() {
     }
 };
 
-getRandomImage();
+ /*
 
 /* const quotes = [
     {
